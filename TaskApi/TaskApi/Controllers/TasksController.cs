7d6eb94a871ebde9ee84a7335303ec978d7db761
diaz-L <lediar.a.diaz@gmail.com>
2021-12-03 04:09:40 -0500

    -created action method to get Task by Id -added object and binaries folder to gitignore

--- TaskApi/TaskApi/Controllers/TasksController.cs
+++ TaskApi/TaskApi/Controllers/TasksController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -30,6 +31,27 @@ namespace TaskApi.Controllers
                 return Ok(tasks);
             }
             catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetTaskById(int id)
+        {
+            try
+            {
+                var task = _context.Tasks
+                    .AsNoTracking()
+                    .Include(t => t.Category)
+                    .FirstOrDefault(t => t.TaskId == id);
+
+                if (task is null)
+                    return NotFound();
+
+                return Ok(task);
+            }
+            catch (Exception e)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
